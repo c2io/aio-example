273c4a3f7dac67523c5b746ec324b28068b79200
chazzhuang <chazzhuang@tecent.com>
2021-06-25 11:50:08 +0800

    session 并发

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+from typing import List
 
 from loguru import logger
 import typer
@@ -19,7 +20,15 @@ def adownload(count: int):
     logger.info(f"{count=},{end_time-start_time=}")
 
 
-download_files = ["http://127.0.0.1:8000/a.txt"] * 1000
+@app.command()
+def adownload2(count: int):
+    start_time = time.time()
+    asyncio.run(aio_download2(count))
+    end_time = time.time()
+    logger.info(f"{count=},{end_time-start_time=}")
+
+
+download_files = ["http://127.0.0.1:8000/a.txt"] * 3000
 log_count = 100
 
 
@@ -29,11 +38,11 @@ def download(count: int = 1):
 
     with requests.Session() as session:
         for index, item in enumerate(download_files):
-            if index % log_count == 0:
-                logger.debug(f"{index=}")
+            # if index % log_count == 0:
+            #     logger.debug(f"{index=}")
             content = session.get(item).content
-            with open(f"data/{index}.txt", "wb") as file:
-                file.write(content)
+            # with open(f"data/{index}.txt", "wb") as file:
+            #     file.write(content)
 
     end_time = time.time()
     logger.info(f"{count=},{end_time-start_time=}")
@@ -52,7 +61,28 @@ async def down(session: aiohttp.ClientSession, url: str):
 async def down_and_write(session: aiohttp.ClientSession, url: str, filename: str):
     response = await session.get(url)
     content = await response.content.read()
-    await write(filename, content)
+    # await write(filename, content)
+
+
+async def down_and_write2(urls: List[str]):
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            response = await session.get(url)
+            content = await response.content.read()  #
+
+
+async def aio_download2(count: int):
+    tasks = list()
+
+    for i in range(count):
+        piece_count = len(download_files) // count
+        start_index, end_index = i * piece_count, (i + 1) * piece_count
+        files = list()
+        for file in download_files[start_index:end_index]:
+            files.append(file)
+        tasks.append(asyncio.create_task(down_and_write2(files)))
+
+    await asyncio.wait(tasks)
 
 
 async def aio_download(count: int):
@@ -60,9 +90,6 @@ async def aio_download(count: int):
     tasks = list()
     async with aiohttp.ClientSession() as session:
         for index, item in enumerate(download_files):
-            if index % log_count == 0:
-                logger.debug(f"{index=}")
-
             tasks.append(
                 asyncio.create_task(down_and_write(session, item, f"data/{index}.txt"))
             )
